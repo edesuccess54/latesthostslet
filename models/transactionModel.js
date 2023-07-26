5da703d5cf9ce7b25507a912744c768b6c05f844
Ede Success <edesuccess54@gmail.com>
2023-07-26 13:32:07 +0100

    changes made

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -1,63 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const transactionSchema = mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    },
-    propertyReference: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Property'
-    },
+const transactionSchema = mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+		},
+		propertyReference: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Property",
+		},
 
-    unitAmount: {
-        type: String,
-        trim: true
-    },
-    
-    amount: {
-        type: Number,
-        required: true,
-        trim: true
-    },
-    method: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    proof: {
-        type: String,
-    },
-    transactionType: {
-        type: String,
-        default: 'Deposit',
-        enum: ['Deposit', 'Withdrawal'],
-        trim: true
-    },
-    wallet: {
-        type: String,
-        trim: true
-    },
+		unitAmount: {
+			type: String,
+			trim: true,
+		},
 
-    status: {
-        type: Boolean,
-        required: true,
-        default: false,
-        trim: true
-    },
+		amount: {
+			type: Number,
+			required: true,
+			trim: true,
+		},
+		method: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		proof: {
+			type: String,
+		},
+		transactionType: {
+			type: String,
+			default: "Deposit",
+			enum: ["Deposit", "Withdrawal"],
+			trim: true,
+		},
+		wallet: {
+			type: String,
+			trim: true,
+		},
 
-    statusMessage: {
-        type: String,
-    },
+		status: {
+			type: Boolean,
+			required: true,
+			default: false,
+			trim: true,
+		},
 
-    datetime: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-})
+		statusMessage: {
+			type: String,
+		},
 
-const Transaction = mongoose.model('transaction', transactionSchema)
+		datetime: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
-module.exports = Transaction
+const Transaction = mongoose.model("transaction", transactionSchema);
+
+module.exports = Transaction;
